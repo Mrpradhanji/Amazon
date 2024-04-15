f43ef50e2aec53bbbf21f9c4e75f7430dfd4fb6b
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-04-15 14:43:33 +0530

    Setting up the Firebase

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA0B64_t6rji0jrHDQE2i3R4vKp3G1c3Bk",
+    authDomain: "clone-b8ef6.firebaseapp.com",
+    projectId: "clone-b8ef6",
+    storageBucket: "clone-b8ef6.appspot.com",
+    messagingSenderId: "186699592823",
+    appId: "1:186699592823:web:9d9fc946a48d4c0b870272",
+    measurementId: "G-Q0032G096C"
+  };
